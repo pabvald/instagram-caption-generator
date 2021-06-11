86db0c4a53846beac223f128b1e7cc6a5832938d
pabvald <valduncielpablo@gmail.com>
2021-06-11 18:17:21 +0200

    Revert "Word embeddings examples"
    This reverts commit 4b66510f43c86018d47251967ef0b593cb0de7d6.

--- data/embeddings/get_embeddings.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# word2vec
-wget -nc "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
-zcat GoogleNews-vectors-negative300.bin.gz > word2vec.bin 
-
-# emoji2vec
-wget -nc  "https://github.com/uclnlp/emoji2vec/raw/master/pre-trained/emoji2vec.bin" 
