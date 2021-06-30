97ae18907941cbe711a0f1cd17f3f9d9dab79a35
heshumi <kkrev.denis98@gmail.com>
2021-06-30 17:34:21 +0200

    Added word2vec_adaptation.py

---
+++ preprocessing/word2vec_adaptation.py
@@ -0,0 +1,14 @@
+from gensim.models import KeyedVectors
+from os.path import join as pjoin, dirname
+import numpy as np
+
+DIR = dirname(__file__)
+PATH_WORD2VEC = pjoin(DIR, '../data/embeddings/word2vec.bin')
+
+# load word2vec and emoji2vec embeddings
+print("\tLoading embeddings...")
+wv = KeyedVectors.load_word2vec_format(PATH_WORD2VEC, binary=True)
+print(len(wv.vocab))
+wv.add(['<start>', '<end>', '<pad>', '<unk>'], [np.random.uniform(0, 1, size = (300)) for i in range(4)])
+print(len(wv.vocab))
+wv.save_word2vec_format(PATH_WORD2VEC[:-4]+'_updated.txt', binary=False)
